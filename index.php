26d07aa427044ff4c918b19f00dfdc69852b4c93
Duncan Mudulo <duntim2@gmail.com>
2016-08-11 15:48:03 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Load Page / html Within Page | Electron</title>
+</head>
+<body>
+
+<!--
+So the HTML4+ specifies an iframe tag that you can use to embed content from 
+another page, for example a youtube video, or a twitter feed, or RSS, here, i will load 
+a youtube page. . . or video link
+-->
+
+<p>
+	This is the default text on the page, say x.com
+</p>
+
+<iframe width="420" height="315"
+src="https://www.youtube.com/watch?v=5Xr4MpBBLTg">
+</iframe>
+
+<!--
+
+The iframe then loads the youtube video at that location
+So its that simple, just an iframe tag
+it can have any url, say www.facebook.com
+
+Then you can style it, position it using css
+
+-->
+
+</body>
+</html>
